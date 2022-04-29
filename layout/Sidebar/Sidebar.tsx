cd0467e4446b92ad162d02ed4dd98413fec819b9
Alexander Bolshakov <paparradoxx@gmail.com>
2022-04-29 15:43:12 +0400

    add card, hhdata components

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 import {SidebarProps} from "./Sidebar.props";
+import styles from './Sidebar.module.css';
 import { Menu } from "../Menu/Menu";
 import Logo from '../logo.svg';
+import cn from 'classnames';
 
-export const Sidebar = ({...props}: SidebarProps): JSX.Element => {
+export const Sidebar = ({className, ...props}: SidebarProps): JSX.Element => {
 	return (
-		<div {...props}>
-			<Logo/>
+		<div className={cn(className, styles.sidebar)} {...props} >
+			<Logo className={styles.logo}/>
 			<div>Поиск</div>
 			<Menu/>
 		</div>	
