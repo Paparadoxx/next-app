6625cfb698ae42adcd8f05d14b025ee9b3ee7e87
Alexander Bolshakov <paparradoxx@gmail.com>
2022-05-11 14:35:48 +0400

    1

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -7,12 +7,18 @@ import { Tag } from "../Tag/Tag";
 import { Button } from "../Button/Button";
 import { declentionOfNum, priceRu } from "../../helpers/helpers";
 import { Divider } from "../Divider/Divider";
+import Image from 'next/image';
 
 export const Product = ({product, className, ...props}: ProductProps): JSX.Element => {
 	return (
 	<Card className={styles.product}>
 		<div className={styles.logo}>
-			<img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title}/>
+			<Image 
+				src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+				alt={product.title}
+				width={70}
+				height={70}
+			/>
 		</div>
 		<div className={styles.title}>{product.title}</div>
 		<div className={styles.price}>
@@ -52,7 +58,7 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
 				<div>{product.disadvantages}</div>
 			</div>}
 		</div>
-		<Divider className={styles.hr}/>
+		<Divider className={cn(styles.hr, styles.hr2)}/>
 		<div className={styles.actions}>
 			<Button appearance="primary">Узнать подробнее</Button>
 			<Button appearance="ghost" arrow="right" className={styles.reviewButton}>Читать отзывы</Button>
